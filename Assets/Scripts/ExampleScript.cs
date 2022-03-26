09ea03d1561461055e76519f2fa1f7c6c65a2504
yodasoda1219 <theo.github@bedafamily.com>
2022-03-25 21:34:07 -0700

    updating along with sge

--- Assets/Scripts/ExampleScript.cs
+++ Assets/Scripts/ExampleScript.cs
@@ -1,5 +1,6 @@
 using SGE;
 using SGE.Components;
+using System.Collections.Generic;
 
 namespace TestProject.Assets.Scripts
 {
@@ -21,6 +22,40 @@ namespace TestProject.Assets.Scripts
 
             var rigidbody = GetComponent<RigidBodyComponent>();
             rigidbody.AddForce(force * modifier);
+        }
+
+        public void OnCollision(Entity entity)
+        {
+            var names = new List<string>();
+
+            var rigidbody = entity.GetComponent<RigidBodyComponent>();
+            var filter = new CollisionFilter(rigidbody);
+
+            int categories = filter.CategoryBits;
+            var categoryNames = Entity.Scene.CollisionCategoryNames;
+
+            for (int i = 0; i < categoryNames.Count; i++)
+            {
+                int mask = 0x1 << i;
+                if ((categories & mask) != 0)
+                {
+                    string name = categoryNames[i];
+                    names.Add(name);
+                }
+            }
+
+            string categoryString = string.Empty;
+            foreach (string name in names)
+            {
+                if (categoryString.Length > 0)
+                {
+                    categoryString += ", ";
+                }
+
+                categoryString += name;
+            }
+
+            Log.Info($"Collided with entity in categories: {categoryString}");
         }
 
         public float Speed { get; set; } = 500f;
